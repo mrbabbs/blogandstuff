a2c3bc15541600da3db01de69184c643d76c9918
Alessandra <alessandra.manna89@gmail.com>
2014-05-27 12:48:47 +0200

    Menu + js

--- assets/js/modernizr.custom.js
+++ assets/js/modernizr.custom.js
@@ -6,3 +6,48 @@
 
 
 
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('#menu').outerHeight();
+
+$(window).scroll(function(event){
+  didScroll = true;
+});
+
+
+setInterval(function() {
+  if (didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
+}, 250);
+
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+       if ($('#menu').is(":visible")) {
+                $('#menu').fadeOut('slow');
+       }
+    } else {
+    	if (($("#menu").is(":visible") === false) || ($(window).scrollTop() + $(window).height() > $(document).height()-100)) {
+       		$('#menu').fadeIn('slow');
+        } 
+    }
+    
+    if(($(document).height()-$(window).height()-$(document).scrollTop()) < 300){
+            ArticleList.loadMoreArticles();
+        } else {
+            console.log('Scroll '+$(document).height()+' - '+$(window).height()+' - ' +$(document).scrollTop());
+        }
+
+    lastScrollTop = st;
+}
